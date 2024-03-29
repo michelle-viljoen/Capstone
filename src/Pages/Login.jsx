fc85ae28be546690a7c5a111e42f954302c22458
Michelle Viljoen <126607925+michelle-viljoen@users.noreply.github.com>
2024-03-29 18:47:13 +0000

    Update Login.jsx correct logo image path

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -95,7 +95,7 @@ return(
   
      <div className="loginPage">
        <h1 className="welcome">Welcome to</h1>
-       <img src=".\src\images\3.png" width="300px" className="logoImage" />
+       <img src=".\images\3.png" width="300px" className="logoImage" />
       <PodcastCarousel podcastData={podcastData} />
     <form onSubmit={handleSubmit} className="loginForm">
         <label htmlFor="email">Email</label>
