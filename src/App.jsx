26f4bc15d6e2dc523d97c7f9519a7a4b9a2bd2b1
Michelle Viljoen <126607925+michelle-viljoen@users.noreply.github.com>
2024-03-29 16:14:15 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { SignUp, Login, Homepage, SingleShow, FavoriteShows } from './pages'
+import { SignUp, Login, Homepage, SingleShow } from './pages'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { supabase } from './supabaseClient'
 import  FavoritesProvider  from './components/FavComponents';
