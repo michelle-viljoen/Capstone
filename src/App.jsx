df477707da5b419961feb5b4f12f1c765263ca6e
Michelle Viljoen <126607925+michelle-viljoen@users.noreply.github.com>
2024-03-29 14:19:00 +0000

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react'
+import { SignUp, Login, Homepage, SingleShow, FavoriteShows } from './pages'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { supabase } from './supabaseClient'
+import  FavoritesProvider  from './components/FavComponents';
+import Favorites from './components/Favorites';
+import AudioPlayer from './components/Audioplayer'
+
+
+const App = () => {
+  const [token, setToken] = useState(false)
+  const [session, setSession] = useState(null)
+
+  if(token) {
+    sessionStorage.setItem('token', JSON.stringify(token))
+  }
+
+  useEffect(() => {
+    if(sessionStorage.getItem('token')){
+      let data = JSON.parse(sessionStorage.getItem('token'))
+      setToken(data)
+    }
+  }, [])
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+//   function PrivateRoute({ children }) {
+//     const { user: token } = useSelector(x => x.auth);
+    
+//     if (!token) {
+//         // not logged in so redirect to login page with the return url
+//         return <Navigate to="/login" state={{ from: history.location }} />
+//     }
+
+//     // authorized so return child components
+//     return children;
+// }
+
+if(!token) {
+  // alert("You need to be logged in to view this content.");
+  <Navigate to="/"/>
+}
+
+
+  return (
+    <div>
+      <AudioPlayer/>
+      <FavoritesProvider> {/* Always render the provider */}
+        <Routes>
+          <Route path={'/signup'} element={<SignUp/>}/>
+          <Route path={'/'} element={<Login setToken={setToken}/>}/>
+          {token && <Route path={'/homepage'} element={<Homepage token={token}/>}/>}
+          {token && <Route path="/podcasts/:id" element={<SingleShow/>} />}
+          <Route path="/favorites" element={<Favorites/>} />
+        </Routes>
+      </FavoritesProvider>
+      <AudioPlayer />
+      
+    </div>
+  )
+}
+
+export default App
